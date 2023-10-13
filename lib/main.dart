e626e207373219f8ed0edf46a654a5bd6bacb0a3
Bruno Gabriel <brunogabryel2003@gmail.com>
2023-10-13 10:54:21 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World! conflito 1'),
+          child: Text('Hello World!'),
         ),
       ),
     );
